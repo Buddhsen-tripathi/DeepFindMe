762f7660ae0c6e4b470e180cf940ed4d0a8de0d8
Buddhsen Tripathi <buddhsent19@gmail.com>
2024-12-04 06:42:04 +0530

    Added vercel speed insights

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,10 +3,12 @@ import NavBar from "@/components/NavBar";
 import FeatureSection from "@/components/FeatureSection";
 import Footer from "@/components/Footer";
 import Sponsor from "@/components/Sponsor";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
+      <SpeedInsights/>
       <NavBar />
       <main className="flex-grow">
         <HeroSection />
